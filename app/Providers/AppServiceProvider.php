3664a7a91d159d32ad4d5971bbb1d9088292c0c8
Abry <vega.antler@gmail.com>
2020-06-23 05:06:56 +0800

    Added Custom Response Factory for API.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\ResponseFactory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Custom response factory
+        ResponseFactory::macro('error', function ($message, $status = 400) {
+            return response()->json([
+                'status_code'   => $status,
+                'message'       => $message,
+            ], $status);
+        });
+
+        ResponseFactory::macro('ok', function ($data = [], $status = 200) {
+            return response()->json([
+                'status_code'   => $status,
+                'data'          => $data,
+            ], $status);
+        });
     }
 }
